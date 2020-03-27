34c1beb7729388ecb06386e12cbcb922c404c22d
akashgujjar <akashsgujjar@gmail.com>
2020-03-27 12:34:44 -0700

    graph and percent change

--- StockBot.py
+++ StockBot.py
@@ -6,20 +6,34 @@ import pandas as pd
 def data_gather(symbol):
     tickerSymbol = symbol
     tickerData = yf.Ticker(tickerSymbol)
-    tickerDf = tickerData.history(period='1d', start='2019-1-1', end='2020-1-1')
-    analyze(tickerDf)
+    tickerDf = tickerData.history(period='1d', start='2017-1-1', end='2020-1-1')
+    print("Drop")
+    analyze_low(tickerDf)
+    print("Up")
+    analyze_high(tickerDf)
     plt.show()
 
 
-def analyze(tickerDf):
+def analyze_low(tickerDf):
     close_price = tickerDf['Close'].to_list()
-    percent_drop = (.05 * close_price[0])
+    percent_drop = (.2 * close_price[0])
     for day in range(close_price.__len__() - 2):
         x = close_price[day]
         y = close_price[day + 1]
         z = close_price[day + 2]
         if x > y > z and (x - z) > percent_drop:
-            print(x, y, z, percent_drop)
+            print(x, y, z)
+
+
+def analyze_high(tickerDf):
+    close_price = tickerDf['Close'].to_list()
+    percent_up = (.1 * close_price[0])
+    for day in range(close_price.__len__() - 2):
+        x = close_price[day]
+        y = close_price[day + 1]
+        z = close_price[day + 2]
+        if x < y < z and (x - z) < percent_up:
+            print(x, y, z)
     tickerDf['Close'].plot()
 
 
