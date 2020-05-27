df0de91226f42bc332fb0268c9c99a1086400bf0
akashgujjar <akashsgujjar@gmail.com>
2020-05-26 21:16:49 -0700

    push

--- StockBot.py
+++ StockBot.py
@@ -1,42 +1,35 @@
-import yfinance as yf
-import matplotlib.pyplot as plt
-from Robinhood import Robinhood
-from flask import Flask
-import pandas as pd
-from datetime import date, datetime, timedelta
-import schedule
 import time
+from datetime import date, timedelta
+import yfinance as yf
+from Robinhood import Robinhood
 
 
 def analyze_market(symbol):
     qr_code = ''
     robinhood_client = Robinhood()
     robinhood_client.login(username='', password='', qr_code=qr_code)
+    confInt = .5
+    confInt += perCheck(symbol, .6) * .5
+
+
+def perCheck(symbol, weight):
     tickerData = yf.Ticker(symbol)
     today = date.today()
     current_date = today.strftime("%Y-%m-%d")
-    start_date_short = (date.today() - timedelta(days=3)).isoformat()
+    start_date_short = (date.today() - timedelta(days=5)).isoformat()
     start_date_long = (date.today() - timedelta(days=20)).isoformat()
     short_term = tickerData.history(period='1d', start=start_date_short, end=current_date)
     long_term = tickerData.history(period='1d', start=start_date_long, end=current_date)
-
-    if analyze_buy(short_term, .05):
-        stock_instrument = robinhood_client.instruments(symbol)[0]
-        # buy_order = robinhood_client.place_market_buy_order(stock_instrument['url'], symbol, 'GFD', 1)
-        print(symbol + " Stock Bought")
-    if analyze_sell(short_term, .05):
-        stock_instrument = robinhood_client.instruments(symbol)[0]
-        # sell_order = robinhood_client.place_market_sell_order(stock_instrument['url'], symbol, 'GFD', 1)
-        print(symbol + " Stock Sold")
-
-    if analyze_buy(long_term, .1):
-        stock_instrument = robinhood_client.instruments(symbol)[0]
-        # buy_order = robinhood_client.place_market_buy_order(stock_instrument['url'], symbol, 'GFD', 2)
-        print(symbol + " Stock Bought")
-    if analyze_sell(long_term, .1):
-        stock_instrument = robinhood_client.instruments(symbol)[0]
-        # sell_order = robinhood_client.place_market_sell_order(stock_instrument['url'], symbol, 'GFD', 2)
-        print(symbol + " Stock Sold")
+    addTo = 0
+    if analyze_buy(short_term, .05, weight / 2):
+        addTo += weight / 2
+    if analyze_buy(long_term, .1, weight / 2):
+        addTo += weight / 2
+    if analyze_sell(short_term, .05, weight / 2):
+        addTo -= weight / 2
+    if analyze_sell(long_term, .1, weight / 2):
+        addTo -= weight / 2
+    return addTo
 
 
 def analyze_buy(tickerDf, percent):
@@ -65,8 +58,14 @@ def analyze_sell(tickerDf, percent):
 
 for day_count in range(6):
     print(time.ctime())
-    analyze_market("PSEC")
-    analyze_market("FSM")
-    analyze_market("EROS")
-    analyze_market("USAS")
+    analyze_market("EBAY")
+    analyze_market("KO")
+    analyze_market("RDFN")
+    analyze_market("HRB")
+    analyze_market("ORCL")
+    analyze_market("MS")
+    analyze_market("CWEN")
+    analyze_market("INTC")
+
+    print("daily check done")
     time.sleep(86400)
